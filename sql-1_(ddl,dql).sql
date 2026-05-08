c92bec19ff25e40f06b6a769cd7bad7856cbac09
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-05-08 09:45:30 +0530

    Update sql-1_(ddl,dql).sql

--- sql-1_(ddl,dql).sql
+++ sql-1_(ddl,dql).sql
@@ -55,3 +55,82 @@ select name,salary from staff;
 -- select * from table where column_name = data;
 select * from staff where name = "lisa";
 select * from staff where emp_id = 2;
+
+
+
+
+
+-- 07/08/2026
+-- functions in sql
+
+-- numeric or math function
+
+-- count()
+select count(*) from staff;
+select count(*) from staff where dept="Hr";
+
+-- avg
+select avg(salary) from staff;
+select avg(salary) from staff where dept = "Manager";
+select dept,avg(salary) from staff group by dept having dept = "Manager";
+
+-- sum()
+select sum(salary) from staff;
+select sum(salary) from staff where dept = "Hr";
+select dept,sum(salary) from staff group by dept;
+
+-- max()
+select max(salary) from staff;
+select dept,max(salary) from staff group by dept;
+select name,max(salary) from staff group by name;
+
+-- mix()
+select min(salary) from staff;
+
+-- text/varchar functions
+-- ucase()
+select ucase(name),dept,salary from staff;
+select ucase(name) as name,dept,salary from staff;
+
+-- char_length()
+select name,char_length(name) from staff;
+
+-- concat()
+select name,concat("RS : ",salary) from staff;
+
+-- format() --> 45,000.000
+select name,concat("RS",format(salary,2)) from staff;
+
+-- left(),right()
+select name,left(name,2) from staff;
+select name,right(name,3) from staff;
+
+
+-- date
+alter table staff add column hire_date date;
+set sql_safe_updates = 0;
+update staff set hire_date = "2024-11-15" where dept="manager";
+update staff set hire_date = "2026-5-15" where dept not in ("manager","hr");
+
+-- common date function
+select now();
+select date(now());
+select time(now());
+
+-- date_format function
+select date_format(curdate(),"%d-%m-%y");
+select date_format(curdate(),"%d/%m/%Y");
+select date_format(curdate(),"%d/%m/%Y") as date;
+
+-- datediff()
+select datediff("2027-1-1",curdate()) as coundown;
+
+-- date_add()
+select date_add(curdate(),interval 2 day);
+select date_add(curdate(),interval -1 day) as yesterday;
+
+select date_add(curdate(),interval -1 month) as yesterday;
+select date_add(curdate(),interval 2 month) as yesterday;
+
+select date_add(curdate(),interval 2 year) as yesterday;
+select date_add(curdate(),interval -2 year) as yesterday;
